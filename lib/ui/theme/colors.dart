7a78f91d364f776c35dc5b2a671c92e84e9de3c2
Ghifari Octaverin <ghifariocta21@gmail.com>
2025-09-11 16:06:21 +0700

    setup project

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+// Grey
+const grey800 = Color(0xFF06122B);
+const grey700 = Color(0xFF818896);
+const grey600 = Color(0xFF9CA3AF);
+const grey500 = Color(0xFFBCC1CA);
+const grey400 = Color(0xFFDDE0E4);
+const grey300 = Color(0xFFE8EBED);
+const grey200 = Color(0xFFF9F9F9);
+const grey100 = Color(0xFFFFFFFF);
+
+// Orange
+const orange800 = Color(0xFFFC4E04);
+const orange700 = Color(0xFFFF570F);
+const orange600 = Color(0xFFFF6C2F);
+const orange500 = Color(0xFFFF7C45);
+const orange400 = Color(0xFFFF8D5E);
+const orange300 = Color(0xFFFFA17A);
+const orange200 = Color(0xFFFFB89A);
+
+// Teal
+const teal800 = Color(0xFF00746D);
+const teal700 = Color(0xFF00847C);
+const teal600 = Color(0xFF00A399);
+const teal500 = Color(0xFF00B6AC);
+const teal400 = Color(0xFF1ED6CB);
+const teal300 = Color(0xFF93E1DD);
+const teal200 = Color(0xFFA2F0EC);
+
+// Primary
+const primary800 = Color(0xFF2B8FEB);
+const primary700 = Color(0xFF4A9BE5);
+const primary600 = Color(0xFF67ACEB);
+const primary500 = Color(0xFF88C1F4);
+const primary400 = Color(0xFFAFD9FF);
+const primary300 = Color(0xFFECF3FA);
+
+// Red
+const red800 = Color(0xFFCB030F);
+const red700 = Color(0xFFE0101C);
+const red600 = Color(0xFFF5121F);
+const red500 = Color(0xFFFF232F);
+const red400 = Color(0xFFFF434D);
+const red300 = Color(0xFFFF6169);
+const red200 = Color(0xFFFF999F);
---
+++ lib/ui/theme/text_styles.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final headlineLarge1 = GoogleFonts.redHatDisplay(
+  fontSize: 42,
+  fontWeight: FontWeight.w700,
+);
+
+final headlineLarge2 = GoogleFonts.redHatDisplay(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+);
+
+final headlineMedium1 = GoogleFonts.redHatDisplay(
+  fontSize: 28,
+  fontWeight: FontWeight.w600,
+);
+
+final headlineMedium2 = GoogleFonts.redHatDisplay(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+final titleLarge = GoogleFonts.redHatDisplay(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+final titleMedium = GoogleFonts.redHatDisplay(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+final titleSmall = GoogleFonts.redHatDisplay(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+final labelLarge = GoogleFonts.redHatDisplay(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+final labelMedium = GoogleFonts.redHatDisplay(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+final labelSmall = GoogleFonts.redHatDisplay(
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+);
+
+final buttonLarge = GoogleFonts.redHatDisplay(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+final buttonMedium = GoogleFonts.redHatDisplay(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+final buttonSmall = GoogleFonts.redHatDisplay(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
+final bodyVeryLarge = GoogleFonts.redHatDisplay(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+final bodyLarge = GoogleFonts.redHatDisplay(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+final bodyMedium = GoogleFonts.redHatDisplay(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+
+final bodySmall = GoogleFonts.redHatDisplay(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+);
